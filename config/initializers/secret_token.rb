abc8d4462990833b1a06b782ff797f39506904d8
Kohei Kadowaki <kadowaki@share-wis.com>
2014-01-18 13:51:27 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pelog::Application.config.secret_key_base = '806524d6f512a059e6eaeb5451c11c4c684675234d9cb53ae56735d3f4001d4512c14a135bfd910bed1ef9208036c6d2f224a5d0f2725c6a8a2e4746a0f49a3d'
