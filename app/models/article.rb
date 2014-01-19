dd06622a20ccc43d06724c0f2915f90a5fa1ea88
Kohei Kadowaki <kadowaki@share-wis.com>
2014-01-19 18:54:09 +0900

    Implement method to extract html from markdown

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,9 +2,12 @@ class Article < ActiveRecord::Base
   FILE_PATH_PATTERN = "#{Rails.root}/app/articles/*.md"
 
   def self.sync
-    self.file_paths.each do |file_path|
-      article_data = self.parse_file_path(file_path)
-      Article.where(article_data).first_or_create
+    file_paths.each do |file_path|
+      article_data = extract_meta_data(file_path)
+      article = Article.where(article_data).first_or_create
+
+      article.body = extract_body(file_path)
+      article.save!
     end
   end
 
@@ -12,7 +15,11 @@ class Article < ActiveRecord::Base
     Dir.glob(FILE_PATH_PATTERN).sort
   end
 
-  def self.parse_file_path(file_path)
+  # Extract meta data of article
+  #
+  # @param file_path [String] markdown file path
+  # @return [Hash] meta data of article
+  def self.extract_meta_data(file_path)
     m = /(\d{4})-(\d{2})-(\d{2})-(.+)\.md/.match(file_path)
     {
       published_year: m[1].to_i,
@@ -20,5 +27,14 @@ class Article < ActiveRecord::Base
       published_date: m[3].to_i,
       slug: m[4]
     }
+  end
+
+  # Extract html body of article
+  #
+  # @param file_path [String] markdown file path
+  # @return [String] html body of article
+  def self.extract_body(file_path)
+    text = File.open(file_path).read
+    Kramdown::Document.new(text, auto_ids: false).to_html
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Article do
   let (:article) { FactoryGirl.create(:article) }
+  let (:file_path) { "#{Rails.root}/spec/articles/2014-01-19-slug.md" }
 
   describe '.sync' do
     before { Article.sync }
@@ -16,11 +17,16 @@ describe Article do
     it { should_not be_empty }
   end
 
-  describe '.parse_file_path' do
-    subject { Article.parse_file_path('app/2014-01-19-slug.md') }
+  describe '.extract_meta_data' do
+    subject { Article.extract_meta_data(file_path) }
     its([:published_year]) { should eq(2014) }
     its([:published_month]) { should eq(1) }
     its([:published_date]) { should eq(19) }
     its([:slug]) { should eq('slug') }
+  end
+
+  describe '.extract_body' do
+    subject { Article.extract_body(file_path) }
+    it { should match(/<h2>/) }
   end
 end
