23d9fb152f29d28d11d334c9191d7e581bf2a38b
Kohei Kadowaki <kadowaki@share-wis.com>
2014-01-21 16:42:38 +0900

    Add github link on Article page

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,10 @@
 class Article < ActiveRecord::Base
+  GITHUB_ARTICLES_URL = 'https://github.com/kadoppe/pelog/tree/master/app/articles/'
+
   default_scope order('published_at DESC')
+
+  # return article url on github repository
+  def github_url
+    GITHUB_ARTICLES_URL + file_name
+  end
 end
