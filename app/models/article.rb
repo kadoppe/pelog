13e9ce22887d206477700d59dbff7f54bd370726
Kohei Kadowaki <kadowaki@share-wis.com>
2014-01-21 17:03:16 +0900

    Fix deprecation default scope

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ActiveRecord::Base
   GITHUB_ARTICLES_URL = 'https://github.com/kadoppe/pelog/tree/master/app/articles/'
 
-  default_scope order('published_at DESC')
+  default_scope { order('published_at DESC') }
 
   # return article url on github repository
   def github_url
