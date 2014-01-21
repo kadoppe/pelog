50125a7e9ba5a1717370647863192f00fcc62b1f
Kohei Kadowaki <kadowaki@share-wis.com>
2014-01-21 14:33:58 +0900

    Implement show action for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,18 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find_by(permalink: permalink_with_format)
+  end
+
+  private
+
+  # Return permalink with format extension
+  def permalink_with_format
+    if params[:format].present?
+      return "#{params[:permalink]}.#{params[:format]}"
+    else
+      return params[:permalink]
+    end
+  end
 end
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe ArticlesController do
-  let (:articles) { [mock_model(Article)] }
+  let (:article) { mock_model(Article) }
+  let (:articles) { [article] }
 
   describe 'GET index' do
     subject { get :index }
     before { Article.stub(:all).and_return(articles) }
     specify { subject and expect(assigns[:articles]).to eq(articles) }
+  end
+
+  describe 'GET show' do
+    subject { get :show, permalink: 'permalink' }
+    before { Article.stub(:find_by).and_return(article) }
+    specify { subject and expect(assigns[:article]).to eq(article) }
   end
 end
