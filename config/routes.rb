d2c219f0d5bd1510f1083752121b37976c4b9be7
Kohei Kadowaki <kadowaki@share-wis.com>
2014-01-23 16:33:39 +0900

    Ignore not html format request to article page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Pelog::Application.routes.draw do
   root to: 'articles#index'
-  get '/:year/:month/:day/:slug', to: 'articles#show', as: 'article'
+
+  scope format: true, constraints: { format: 'html' } do
+    get '/:year/:month/:day/:slug',
+      to: 'articles#show',
+      as: 'article',
+      constraints: {
+        year: /\d{4}/,
+        month: /\d{2}/,
+        day: /\d{2}/,
+      }
+  end
 
   get '/feed', to: 'articles#feed'
   get '/rss', to: redirect('/feed')
